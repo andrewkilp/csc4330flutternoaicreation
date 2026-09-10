a8a68635e8f52fb1634d3bb293a1a06fad827f3a
LAPTOP-BPMQJP10\andyk <andykilp96@gmail.com>
2026-09-10 16:05:12 -0500

    updated text size

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             const Text('Andrew has pushed the button this many times:',
               style: TextStyle(
-                fontSize: 124.0, // Adjust this number to make it bigger
+                fontSize: 120.0, // Adjust this number to make it bigger
                 fontWeight: FontWeight.bold, // Optional: makes it bold too
               ),
             ),
